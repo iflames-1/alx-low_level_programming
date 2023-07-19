b993b382536b6e1dc6b348bacf3ec3a2b2af5a2a
Isaac <isaacomenuche@gmail.com>
2023-07-18 21:26:20 -0700

    fix some betty documentation issue

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * print_fibonacci(int n) - this program prints the first 50
+ * main - this program prints the first 50
  * Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0 (Succes)
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,28 +13,22 @@ void times_table(void)
 		for (column = 0; column <= 9; column++)
 		{
 			product = row * column;
-
-			/* Print comma and space before each number, except the first */
-			if (column != 0)
+			if (column == 0)
 			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-			/* Print the product as a two-digit number */
-			if (product <= 9)
-			{
-				_putchar(' ');
 				_putchar(product + '0');
 			}
 			else
 			{
-				_putchar((product / 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 9)
+					_putchar(' ');
+				else
+					_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
 			}
 		}
-
-		/* Move to the next row */
 		_putchar('\n');
 	}
 }
+
