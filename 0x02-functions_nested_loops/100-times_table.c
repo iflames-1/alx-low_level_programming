61844935e8a37e7aca5167bc44903f7345c71446
Isaac <isaacomenuche@gmail.com>
2023-07-19 04:46:26 -0700

    made changes on 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,18 +10,36 @@ void print_times_table(int n)
 {
 	int i, j, product;
 
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			product = i * j;
-			if (j > 0)
-				printf(", ");
-			printf("%d", product);
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				product = i * j;
+				if (product <= 99)
+					_putchar(' ');
+				if (product <= 9)
+					_putchar(' ');
+				if (product >= 100)
+				{
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10)) % 10 + '0');
+				}
+				else if (product <= 99 && product > 9)
+				{
+					_putchar((product / 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				_putchar((product % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		printf("\n");
 	}
 }
