19366090df1d452cb2e4eb2e54e4650984c318e6
Isaac <isaacomenuche@gmail.com>
2023-07-21 09:57:40 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,42 +1,33 @@
 #include "main.h"
 
 /**
+ * print_positive_number - Helper function to print a positive integer
+ * @n: The positive integer to print
+ */
+void print_positive_number(unsigned int n)
+{
+	if (n / 10 != 0)
+		print_positive_number(n / 10);
+	_putchar(n % 10 + '0');
+}
+
+/**
  * print_number - Prints an integer.
  * @n: The integer to print.
  */
 void print_number(int n)
 {
-	int divisor = 1;
-	int num_digits = 0;
-	int i;
-	int num;
-
-	/* Handle negative numbers */
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		print_positive_number(-n);
 	}
-
-	/* Count the number of digits in the integer */
-	num = n;
-	while (num != 0)
+	else if (n == 0)
 	{
-		num /= 10;
-		num_digits++;
+		_putchar('0');
 	}
-
-	/* Calculate the divisor based on the number of digits */
-	for (i = 1; i < num_digits; i++)
+	else
 	{
-		divisor *= 10;
-	}
-
-	/* Print each digit one by one */
-	while (divisor > 0)
-	{
-		_putchar((n / divisor) + '0');
-		n %= divisor;
-		divisor /= 10;
+		print_positive_number(n);
 	}
 }
