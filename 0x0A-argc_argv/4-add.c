ebb3889985d4a11273829f6a72ccb28da3a8bfea
Isaac <isaacomenuche@gmail.com>
2023-10-02 20:45:25 +0100

    program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - program that adds only positive numbers togeter
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 if no argument is passed or success
+ *1 if argument is'nt a positive number
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		char *arg = argv[i];
+		int num = atoi(argv[i]);
+		/*Check if atoi encountered non-numeric input*/
+		if (num == 0 && arg[0] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		/*Check if the number is negative*/
+		if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
