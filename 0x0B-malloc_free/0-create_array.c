5b6f1c71ee4def5ea2a29970fb5333a89dbdc5da
Isaac <isaacomenuche@gmail.com>
2023-10-03 09:18:31 +0100

    betty coding style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ *create_array - creates an array of chars,
+ *and initializes it with a specific char
+ *@size: size of char
+ *@c: char
+ *Return: pointer to the array, or NULL if it fails
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *ar;
 	unsigned int i = 0;
+
 	if (size == 0)
 	{
 		return (NULL);
@@ -12,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	ar = malloc(sizeof(char) * size);
 	if (ar == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	while (i < size)
 	{
@@ -20,5 +28,5 @@ char *create_array(unsigned int size, char c)
 		i++;
 	}
 
-	return ar;
+	return (ar);
 }
