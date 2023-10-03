75e37c37880ff56d60175e2373c2e9dfbb29827f
Isaac <isaacomenuche@gmail.com>
2023-10-03 09:13:13 +0100

    array of char intialized to specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i = 0;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ar = malloc(sizeof(char) * size);
+	if (ar == NULL)
+	{
+		return NULL;
+	}
+	while (i < size)
+	{
+		ar[i] = c;
+		i++;
+	}
+
+	return ar;
+}
