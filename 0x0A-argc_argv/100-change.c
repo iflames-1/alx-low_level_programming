707e838e39d85f6efd40bc4245752813a2a68221
Isaac <isaacomenuche@gmail.com>
2023-10-08 16:58:28 +0100

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,39 +12,28 @@ int main(int argc, char *argv[])
 {
 	int num = atoi(argv[1]);
 	int cents = 0;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (cents < 0)
+	if (num < 0)
 	{
-		printf("0\n");
+		printf("Error\n");
 		return (0);
 	}
 	while (num > 0)
 	{
 		if (num >= 25)
-		{
 			num -= 25;
-		}
 		else if (num >= 10)
-		{
 			num -= 10;
-		}
 		else if (num >= 5)
-		{
 			num -= 5;
-		}
 		else if (num >= 2)
-		{
 			num -= 2;
-		}
 		else
-		{
 			num -= 1;
-		}
 		cents++;
 	}
 
