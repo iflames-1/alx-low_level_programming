d65040e1627849df7d22cd2dfcba8e3c7375fd45
Isaac <isaacomenuche@gmail.com>
2023-10-12 00:22:55 +0100

    prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - reads the code
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: result from operation
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	unsigned char *main_ptr = (unsigned char *)&main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_ptr[i]);
+
+		if (i < num_bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
