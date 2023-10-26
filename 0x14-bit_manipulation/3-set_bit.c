d1dd7c1a28a30c34b3cd6bac8e5aa4e3c36b00d0
Isaac <isaacomenuche@gmail.com>
2023-10-26 20:21:52 +0100

    Add 5-flip_bits.c 20:21 10/26/23

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
 
 /**
  *set_bit - function that sets the value of a bit to 1 at a given index.
- *@n: number
+ *@n: pointer to number
  *@index: the index, starting from 0 of the bit you want to set
  *Return: 1 if it worked, or -1 if an error occurred
  */
