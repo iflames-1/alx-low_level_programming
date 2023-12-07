29ee00a4fd062d2e9ddbc6a8b6acc5fc8ac6d1b0
Isaac <isaacomenuche@gmail.com>
2023-12-07 15:25:10 +0100

    add betty doc

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: head node
+ * @n: n element in node
+ * Return: pointer to new node
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *current = *head;
