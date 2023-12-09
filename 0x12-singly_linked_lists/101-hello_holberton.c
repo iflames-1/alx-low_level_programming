39523e2335bc6355ae425e08e9e2259620d6e49a
Flames <isaacomenuche@gmail.com>
2023-12-10 00:19:57 +0100

    assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * main - generate assembly code
+ * gcc -S -masm=intel 101-hello_holberton.c -o 101-hello_holberton.asm
+ */
+void main(void)
+{
+	printf("Hello, Holberton\n");
+}
