d3990d6080ee8b958300f04a77e8dc1c29225d3d
Flames <isaacomenuche@gmail.com>
2023-12-10 10:00:02 +0100

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,8 +9,8 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new_node, *temp_node = *head;
+	unsigned int i = 0;
+	listint_t *new_node, *current = *head;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -25,17 +25,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	for (i = 0; temp_node != NULL; i++)
+	while (current != NULL && i < idx - 1)
 	{
-		if (i == (idx - 1))
-		{
-			new_node->next = temp_node->next;
-			temp_node->next = new_node;
-			return (new_node);
-		}
-		temp_node = temp_node->next;
+		current = current->next;
+		i++;
 	}
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = current->next;
+	current->next = new_node;
 
-	free(new_node);
-	return (NULL);
+	return (new_node);
 }
