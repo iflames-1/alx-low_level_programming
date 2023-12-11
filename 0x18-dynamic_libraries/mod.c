9d424642889ea2805015831b5fc03dc36d396765
Isaac <isaacomenuche@gmail.com>
2023-12-11 20:00:47 +0100

    C + Python

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+/**
+ * mod - add a and b
+ * @a: first num
+ * @b: second num
+ * Return: mod
+ */
+int mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - add a and b
+ * @a: first num
+ * @b: second num
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
