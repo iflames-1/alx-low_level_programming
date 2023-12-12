1dc6d98c6fe1fb77b3d53b2568f1de39061279b2
Flames <isaacomenuche@gmail.com>
2023-12-12 01:05:58 +0100

    Hello Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,14 @@
 section .data
-    hello db 'Hello, Holberton', 0
+    format db 'Hello, Holberton', 10, 0  ; 10 is the ASCII code for newline
 
 section .text
     global main
-    extern puts
+    extern printf
 
 main:
     push rdi
-    lea rdi, [hello]
-    call puts
+    lea rdi, [format]
+    call printf
     pop rdi
     mov eax, 0
     ret
