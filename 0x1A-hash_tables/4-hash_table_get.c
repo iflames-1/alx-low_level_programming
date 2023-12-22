3ccd2633f74a17674a93c46cf1e74d0b2e0b05c4
Flames <isaacomenuche@gmail.com>
2023-12-22 18:53:44 +0100

    retrieve a value with key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves value associated with a key
+ * @ht: hash table we are looking into
+ * @key: key we are looking for
+ * Return: the value associated with key or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *node = ht->array[index];
+
+	if (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		while (node != NULL)
+		{
+			if (strcmp(node->key, key) == 0)
+				return (node->value);
+			if (node->next == NULL)
+				break;
+			node = node->next;
+		}
+	}
+	return (NULL);
+}
