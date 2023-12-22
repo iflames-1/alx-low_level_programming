b5fb43f6850e42a49597db8c831bb43edf6c5e6e
Flames <isaacomenuche@gmail.com>
2023-12-22 22:48:19 +0100

    comma

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,21 +7,22 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
-	hash_table_t *current = (hash_table_t *)ht;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
 
 	printf("{");
-	while (i < ht->size - 1)
+	while (i < ht->size)
 	{
-		while(current->array[i] != NULL)
+		current = ht->array[i];
+		while (current != NULL)
 		{
-			if (current->array[i]->key)
-			{
-				printf("'%s': '%s'", current->array[i]->key, current->array[i]->value);
+			printf("'%s': '%s'", current->key, current->value);
 
-				if (current->array[i]->next != NULL || i < ht->size - 1)
-					printf(", ");
-			}
-			current->array[i] = current->array[i]->next;
+			printf(", ");
+
+			current = current->next;
 		}
 		i++;
 	}
